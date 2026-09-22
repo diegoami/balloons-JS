272270a74455a360f6228ca67851760ca3890eba
Diego Amicabile <diego.amicabile@gmail.com>
2026-09-23 01:56:10 +0200

    Release 2.4: versionCode 5, versionName 2.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // a WebView new enough to run this without surprises.
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.3"
+        versionCode = 5
+        versionName = "2.4"
     }
 
     if (hasSigning) {
