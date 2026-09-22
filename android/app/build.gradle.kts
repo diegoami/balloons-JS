c9c2a371b2249291ad2a9931de66e151c7e7e94a
Diego Amicabile <diego.amicabile@gmail.com>
2026-09-22 10:15:37 +0200

    Release 2.2: versionCode 3, versionName 2.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // a WebView new enough to run this without surprises.
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.1"
+        versionCode = 3
+        versionName = "2.2"
     }
 
     if (hasSigning) {
